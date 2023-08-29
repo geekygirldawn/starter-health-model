2ed31a0ecedd33d3cd774710b3277df58f23144a
Dawn M. Foster <dawn@dawnfoster.com>
2023-08-29 07:33:53 +0100

    printed message to the screen to indicate that it is creating the environment and fixed a typo

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,5 +1,7 @@
 #!/bin bash
 
+echo "Creating virtual env"
 python3 -m venv .venv
+
 echo "To activate virtual env: \"source .venv/bin/activate\""
-echo "To install dependencies: \"pip install -r requirements.txt"
+echo "To install dependencies: \"pip install -r requirements.txt\""
